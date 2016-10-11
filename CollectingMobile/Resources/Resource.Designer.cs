983592ead1342c564a26a2de4e4db5591c2ae162
EugenStimac <eugen.stimac@gmail.com>
2016-10-11 13:11:50 +0200

    proj changes

--- CollectingMobile/Resources/Resource.Designer.cs
+++ CollectingMobile/Resources/Resource.Designer.cs
@@ -122,25 +122,16 @@ namespace CollectingMobile
 			public const int Login = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int LoginNoToolbar = 2130903041;
+			public const int RequestListItem = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int RequestListItem = 2130903042;
+			public const int Requests = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int Requests = 2130903043;
+			public const int Specimens = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int RequestsNoToolbar = 2130903044;
-			
-			// aapt resource value: 0x7f030005
-			public const int Specimens = 2130903045;
-			
-			// aapt resource value: 0x7f030006
-			public const int SpecimensNoToolbar = 2130903046;
-			
-			// aapt resource value: 0x7f030007
-			public const int toolbar = 2130903047;
+			public const int toolbar = 2130903044;
 			
 			static Layout()
 			{

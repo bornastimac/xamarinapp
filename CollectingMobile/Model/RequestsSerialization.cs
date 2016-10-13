02967061f1df08d6ec7e902b622c88cc2507baa9
EugenStimac <eugen.stimac@gmail.com>
2016-10-13 21:54:13 +0200

    serialization/deserialization functionality for requests

--- CollectingMobile/Model/RequestsSerialization.cs
+++ CollectingMobile/Model/RequestsSerialization.cs
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-=======
 using System.Collections.Generic;
 using System.Linq;
 using Android.Content;
->>>>>>> serialization/deserialization functionality for requests
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
